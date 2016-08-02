52a0e2f015e22488a5c57d5682963c05d75f6089
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-02 11:52:58 -0500

    .length

--- app.js
+++ app.js
@@ -4,10 +4,20 @@
 var name = prompt("Enter your name:", "");
 document.write("Hello " + name);
 
-var stuff = new Array(3);
+var stuff = new Array(1);
 
 for(i=0; i<3; i++){
   stuff[i] = prompt("Add something to the array: ", "");
 }
 
 document.write(stuff[0] + stuff[1] + stuff[2]);
+
+/****************************************************
+*       Cool Technique to Print Array Elements      *
+****************************************************/
+var food = new Array("apples", "pears", "bacon", "tuna", "ham");
+food.sort();
+
+for(i=0; i < food.length; i++) {
+  document.write(food[i] + "<br>");
+}
